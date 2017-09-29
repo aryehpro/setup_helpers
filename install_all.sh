fb7b1ea3f2780e2dc0ba1150d6fd429211d91c96
Aryeh Promislow <aryehpro@gmail.com>
2017-09-29 18:20:43 +0300

    Fix sudo issues

--- install_all.sh
+++ install_all.sh
@@ -1,16 +1,10 @@
 #! /bin/bash
 
-# We need sudo to do some stuff
-if [[ $EUID -ne 0 ]]; then
-   echo "Please run as root."
-   exit 1
-fi
-
 read -p "enter desired pycharm version" pycharm_version
 read -p "enter email address" email
 
-./install_chrome.sh
-./install_pycharm.sh $pycharm_version
+sudo ./install_chrome.sh
+sudo ./install_pycharm.sh $pycharm_version
 
 ./install_knesset_dpp_deps.sh
 
